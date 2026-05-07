516907bf2c8c03c662c97e9e7cddb1a8b26d4a40
AzanoRivers <job.devcode@gmail.com>
2026-05-06 20:18:51 -0500

    fix auth protecction subdomain

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -101,7 +101,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: 'lax',
         path: '/',
         secure: process.env.NODE_ENV === 'production',
-        domain: process.env.NODE_ENV === 'production' ? '.battleboost.pro' : undefined,
+        domain: process.env.NEXTAUTH_COOKIE_DOMAIN ?? undefined,
       },
     },
   },
